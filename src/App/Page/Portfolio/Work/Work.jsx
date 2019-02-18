f2790c1d20f42d05edbf2053c2d91a217b3c5a9b
Evelyn <ks0430@163.com>
2019-02-19 00:40:22 +1000

    Add work card grid layout. Init work card component.

--- src/App/Page/Portfolio/Work/Work.jsx
+++ src/App/Page/Portfolio/Work/Work.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './Work.scss';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import WorkCard from '../../../Common/Card/WorkCard';
 
 export default class Work extends Component {
 
-  
+
 
   onClick = () => {
     console.log("click!");
@@ -22,6 +23,24 @@ export default class Work extends Component {
             <NavLink className="section-nav__link" href="#">Link2</NavLink>
           </NavItem>
         </Nav>
+        <div className="work-grid">
+          <div className="work-grid__item">
+            <WorkCard />
+          </div>
+          <div className="work-grid__item">
+            <WorkCard />
+          </div>
+          <div className="work-grid__item">
+            <WorkCard />
+          </div>
+          <div className="work-grid__item">
+            <WorkCard />
+          </div>
+          <div className="work-grid__item">
+            <WorkCard />
+          </div>
+
+        </div>
       </div>
     )
   }
