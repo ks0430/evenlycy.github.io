350ee5b7169f4d458b2a08dd9c79fc6365e8fe9a
Evelyn <ks0430@163.com>
2019-02-18 16:16:13 +1000

    Add word shake animation on main title.

--- src/App/Common/MainTitle/MainTitle.jsx
+++ src/App/Common/MainTitle/MainTitle.jsx
@@ -4,7 +4,7 @@ import "./MainTitle.scss";
 const MainTitle = ({title,path}) => {
   return (
     <div className="main-title">
-      <h1 className="main-title__title">{title}</h1>
+      <h1 className="main-title__title" data-text={title}>{title}</h1>
       <p className="main-title__path">{path}</p>
     </div>
   )
