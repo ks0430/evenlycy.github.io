deb0b2ebcb936fc4b6253247fe9e9102cc9880f6
Evelyn <ks0430@163.com>
2019-02-19 15:35:34 +1000

    Add card styles. Add mixin: abcCenter() & underline()

--- src/App/Page/Portfolio/Work/Work.jsx
+++ src/App/Page/Portfolio/Work/Work.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './Work.scss';
-import WorkCard from '../../../Common/Card/WorkCard';
+import Card from '../../../Common/Card/Card';
 import SubNav from '../../../Common/Nav/SubNav';
 import SubTitle from '../../../Common/MainTitle/SubTitle';
 export default class Work extends Component {
@@ -14,22 +14,23 @@ export default class Work extends Component {
       <div className="work">
         <SubTitle>Recent Works</SubTitle>
         <SubNav onClick={this.onClick} />
+
         <div className="work-grid">
           <div className="work-grid__item">
-            <WorkCard />
+            <Card 
+              imgUrl="https://images.unsplash.com/photo-1506994011460-5482746d30a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+              type="Web App"
+              title="A music store"
+            />
           </div>
           <div className="work-grid__item">
-            <WorkCard />
+            <Card 
+              imgUrl="https://images.unsplash.com/photo-1506994011460-5482746d30a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+              type="Web App"
+              title="A music store"
+            />
           </div>
-          <div className="work-grid__item">
-            <WorkCard />
-          </div>
-          <div className="work-grid__item">
-            <WorkCard />
-          </div>
-          <div className="work-grid__item">
-            <WorkCard />
-          </div>
+
         </div>
       </div>
     )
