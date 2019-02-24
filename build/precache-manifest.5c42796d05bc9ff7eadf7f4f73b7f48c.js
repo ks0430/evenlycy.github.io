d7129fa421c5c50e3954e071ca776cfe52957886
Evelyn <ks0430@163.com>
2019-02-25 00:22:30 +1000

    Merge github deployed static page.

---
+++ build/precache-manifest.5c42796d05bc9ff7eadf7f4f73b7f48c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "04930ea31616e88eec284585cfa3421c",
+    "url": "/static/media/vrmainmed.04930ea3.png"
+  },
+  {
+    "revision": "c30e4dd55ebf095d5234f97e9dd9a81a",
+    "url": "/static/media/sprite.c30e4dd5.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9906957047f29d6e988d",
+    "url": "/static/js/main.99069570.chunk.js"
+  },
+  {
+    "revision": "105322533408788d63b1",
+    "url": "/static/js/1.10532253.chunk.js"
+  },
+  {
+    "revision": "9906957047f29d6e988d",
+    "url": "/static/css/main.c97ce1bf.chunk.css"
+  },
+  {
+    "revision": "105322533408788d63b1",
+    "url": "/static/css/1.c8625538.chunk.css"
+  },
+  {
+    "revision": "56ab69b0124e43e9c7bbe4d557ee0200",
+    "url": "/index.html"
+  }
+];
